87b461ad71223cf01ef1caa1c14b1fc6b98e1438
Edson Grifo <41455569+edsongrifo@users.noreply.github.com>
2023-03-25 14:54:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,21 @@
 
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+capabilities = webdriver.DesiredCapabilities().CHROME.copy()
+capabilities['acceptInsecureCerts'] = True
 
 servico = Service(ChromeDriverManager().install())
 
-
-
-driver = webdriver.Chrome(executable_path="/opt/render/project/.render/chrome/opt/google/chrome/google-chrome/chromedriver",
-                                   desired_capabilities=capabilities,options=chrome_options)
+navegador = webdriver.Chrome(service=servico, desired_capabilities=capabilities, options=chrome_options)
 
 navegador = webdriver.Chrome(service=servico)
 
