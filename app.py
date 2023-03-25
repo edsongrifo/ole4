c00945df43afc65a3835c40f1ebcd5fe6082d787
Edson Grifo <41455569+edsongrifo@users.noreply.github.com>
2023-03-25 15:14:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-import win32com.client as win32
 
 servico = Service(ChromeDriverManager().install())
+
+
+
+driver = webdriver.Chrome(executable_path="/opt/render/project/.render/chrome/opt/google/chrome/google-chrome/chromedriver",
+                                   desired_capabilities=capabilities,options=chrome_options)
 
 navegador = webdriver.Chrome(service=servico)
 
